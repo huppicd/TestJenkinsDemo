54eb484c3cf466f474c9946c278e2b02fd58610d
Hupendra Deore <FD00422@fulcrumdigital.com>
2023-02-21 11:21:05 +0530

    demo commit

---
+++ src/test/java/QA_FD_Maven/Additons.java
@@ -0,0 +1,18 @@
+package QA_FD_Maven;
+
+import org.apache.logging.log4j.core.util.SystemNanoClock;
+
+public class Additons {
+
+	int a;
+	int b;
+	
+	public int addtion(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+		return c;
+	}
+	
+	
+	
+}
---
+++ src/test/java/QA_FD_Maven/Main.java
@@ -0,0 +1,15 @@
+package QA_FD_Maven;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Additons add=new Additons();
+		
+		
+		add.addtion(5, 9);
+
+	}
+
+}
