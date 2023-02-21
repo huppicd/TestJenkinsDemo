961a817b014a86d713c6cc010526f7b101c514da
huppicd@gmail.com <FD00422@fulcrumdigital.com>
2023-02-21 19:51:21 +0530

    updating addition code

--- src/test/java/QA_FD_Maven/Additons.java
+++ src/test/java/QA_FD_Maven/Additons.java
@@ -1,18 +1,12 @@
 package QA_FD_Maven;
 
-import org.apache.logging.log4j.core.util.SystemNanoClock;
-
 public class Additons {
 
-	int a;
-	int b;
-	
-	public int addtion(int a, int b) {
-		int c=a+b;
-		System.out.println(c);
-		return c;
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 23;
+
+        System.out.println(a + b);
 	}
-	
-	
-	
+
 }
